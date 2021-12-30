a9ee634ab8258a4f579f73c175a0ff253733f38a
reva_mariia <revafedenkom@gmail.com>
2021-12-31 01:03:39 +0200

    add hw_6_3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'dogs/index'
+  get 'dogs/show'
+  get 'dogs/new'
+  get 'dogs/edit'
   resources :microposts
   resources :users
   root to: 'users#index'
 
+  resources :dogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
